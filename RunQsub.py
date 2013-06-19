b619e14114a491b46a83bc19d2ddd15262bcaa67
jschlitt84 <jslongboards@gmail.com>
2013-06-19 13:04:02 -0400

    Added quick stopAt value to RunQsub to test smaller subsets

--- RunQsub.py
+++ RunQsub.py
@@ -6,6 +6,7 @@ import select
 increment = 10
 submitDelay = 3
 inputDelay = 10
+stopAt = 10
 
 print "Loading qsubs..."
 tempFile = open("qsublist")
@@ -16,16 +17,19 @@ length = len(script)
 
 print "%s qsubs found, running %s per user prompt with a %s second input delay and a %s second submission delay" % (length,increment,inputDelay,submitDelay) 
 
-pos1 = 0
-while pos1 < length:    
-
-    print script[pos1]
-    subprocess.call(script[pos1])    
+pos = 0
+while pos < length:    
     
-    print "Submitted Qsub", pos1, "out of", length-1
+    if pos == stopAt:
+        quit()
+        
+    print script[pos]
+    subprocess.call(script[pos])    
+    
+    print "Submitted Qsub", pos, "out of", length-1
     time.sleep(submitDelay)
     
-    if pos1 > 0 and pos1%increment == 0:
+    if pos > 0 and pos%increment == 0:
         print "Interval Reached, please enter 'quit' in next", inputDelay, "seconds to quit"
         i, o, e = select.select([sys.stdin], [], [], inputDelay)
         if (i):
@@ -34,7 +38,7 @@ while pos1 < length:
                 quit()
             else:
                 print "No input recieved, continuing process"
-    pos1 += 1
+    pos += 1
         
 print "Process complete, quitting now"
 quit()
