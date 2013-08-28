e4b70627007d13633d16d9b87142ba0aaecb1d5c
jschlitt84 <jslongboards@gmail.com>
2013-08-28 15:15:27 -0400

    tweak

--- GetSliceMultiCore.py
+++ GetSliceMultiCore.py
@@ -373,7 +373,7 @@ def checkLines(fileName, subpopLoaded, useSubpop, multiThreaded):
         p.join()
         
     for key, entry in merged.iteritems():
-        print entry
+        print "ENTRY:",entry
         days =  max(days, entry[2])
         
     comments = merged['comments']
