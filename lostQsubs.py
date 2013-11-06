73f5fa5c5373cfc7e39539526e7a8dbc5ff58c68
James Schlitt <jslongboards@gmail.com>
2013-11-05 22:18:17 -0500

    Update lostQsubs.py

--- lostQsubs.py
+++ lostQsubs.py
@@ -7,9 +7,13 @@ list1 = file1.readlines(); file1.close()
 list2 = file2.readlines(); file2.close()
 dir = list1[0].replace('qsub ','')[0:list1[0].index('/'.split(list2[0])[0])]
 
+print "Dir:", dir
+
+list2 =  list2[0:5]
+
 for pos in range(len(list2)):
   line = list2[pos]
-  list2[pos] = 'qsub ' + dir + ' '.split(line)[0] + '/qsub'
+  list2[pos] = 'qsub ' + dir + ' '.split(line)[0] + '/qsub\n'
   print list2[pos]
   
 for line in list2:
