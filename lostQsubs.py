77788bf7af1517a2dc10ab00b45aa46140e2e602
James Schlitt <jslongboards@gmail.com>
2013-11-05 22:11:13 -0500

    Create lostQsubs.py

---
+++ lostQsubs.py
@@ -0,0 +1,20 @@
+import sys, os
+
+file1 = open(sys.argv[1])
+file2 = open(sys.argv[2])
+fileOut = open(sys.argv[3],'a+b')
+list1 = file1.readlines(); file1.close()
+list2 = file2.readlines(); file2.close()
+dir = list1[0].replace('qsub ','')[0:list1[0].index('/'.split(list2[0])[0])
+
+for pos in range(len(list2)):
+  line = list2[pos]
+  list2[pos] = 'qsub ' + dir + ' '.split(line)[0] + '/qsub'
+  print list2[pos]
+  
+for line in list2:
+  if line not in list1:
+    print "Missing:", line
+    fileOut.write(line)
+    
+fileOut.close()
