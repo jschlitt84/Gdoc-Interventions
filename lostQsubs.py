aa27fcecee8b4e93b39c2b0d5c72acaf0141f75e
James Schlitt <jslongboards@gmail.com>
2013-11-05 22:25:31 -0500

    Update lostQsubs.py

--- lostQsubs.py
+++ lostQsubs.py
@@ -7,7 +7,9 @@ list1 = file1.readlines(); file1.close()
 list2 = file2.readlines(); file2.close()
 print list1[0:5]
 print list2[0:5]
-dir = list1[0].replace('qsub ','')[0:list1[0].index('/'.split(list2[1])[0])]
+print '/'.split(list2[1])[0]
+print list1[0][0:list1[0].index('/'.split(list2[1])[0])]
+dir = list1[0][0:list1[0].index('/'.split(list2[1])[0])].replace('qsub ','')
 
 print "Dir:", dir
 
