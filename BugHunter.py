38e7956c90cb5754f4088a058a6a0fb55672eee2
James Schlitt <jslongboards@gmail.com>
2013-11-14 13:49:39 -0500

    tweak

--- BugHunter.py
+++ BugHunter.py
@@ -575,7 +575,7 @@ def main():
                             'fromType':subpopFiles[subpop[1] + '_type'],
                             'fromName':subpop[1]}
             print "\nAnalizing crosstalk for", experiment[1], "\n\twith subpops", subpop[0:2]
-            crossTalk = loadCrossTalk(crossTalkEFO6, crossTalkSubs, durations[experiment[1]])
+            crossTalk = loadCrossTalk(crossTalkEFO6, crossTalkSubs, durations[experiment[0]])
             statsOut = open(outDir + experiment[0],'a+b')
             statsOut.write(curvesToString(crossTalk['meanCrossTalkCurve'],
                                             crossTalk['crossTalkCurves'],
